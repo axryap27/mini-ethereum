aa6e68f0a9616129a73343dc755636159acfdadc
aarya <aarya27@gmail.com>
2025-05-02 16:32:39 -0500

    blockchain constructor and genesis block creation

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+import json
+from typing import List, Dict, Any
+from datetime import datetime
+from block import Block
+
+
+class Blockchain:
+   def __init__(self, difficulty: int = 2):
+       self.chain: List[Block] = []
+       self.pending_transactions: List[Dict[str, Any]] = []
+       self.difficulty = difficulty
+       self.mining_reward = 10.0
+       self.create_genesis_block()
+      
+
+
+   def create_genesis_block(self) -> None:
+       genesis_block = Block(
+           index=0,
+           transactions=[],
+           timestamp=datetime.now().timestamp(),
+           previous_hash="0" * 64
+       )
+       genesis_block.mine_block(self.difficulty)
+       self.chain.append(genesis_block)
